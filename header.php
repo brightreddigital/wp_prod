0b4745f9d28cb04ea1ec6f6c607407854ec2d0bf
Robert Clarke <robert@brightredmedia.com>
2023-07-24 13:29:28 +0100

    Update header.php

--- header.php
+++ header.php
@@ -13,12 +13,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php if( get_field('typekit', 'option') ): ?>
+	<?php if( get_field('acf-field_63d27fd387106', 'option') ): ?>
 		<link rel="stylesheet" href="https://use.typekit.net/<?php the_field('typekit', 'option'); ?>.css">
 		<script type="text/javascript">console.log('typekit')</script>
 	<?php endif; ?>
 
-	<?php if( get_field('google_font', 'option') ): ?>
+	<?php if( get_field('acf-field_63d27efc87105', 'option') ): ?>
 		<?php the_field('google_font', 'option'); ?>
 	<?php endif; 
 
